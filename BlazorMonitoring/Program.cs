acd50d48fa607491b069db2d7f2d16bcca542734
EiK <231216@usn.no>
2022-09-21 15:19:17 +0200

    Removed all spaceX API related test code

--- BlazorMonitoring/Program.cs
+++ BlazorMonitoring/Program.cs
@@ -1,5 +1,4 @@
 using FM4017Library.DataServices;
-using FM4017Library.DataServices.SpaceX;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,19 +7,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
-
-
-//SpaceX (just for test)
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["spacex_api_base_url"]) });
-builder.Services.AddHttpClient<ISpaceXDataService, GraphQLSpaceXDataService>
-    (spds => spds.BaseAddress = new Uri(builder.Configuration["spacex_api_base_url"]));
-
+// Using httpclient factory to register a dataservice requesting an interface
+// Making the service available for dependency injection (DI), giving loose coupling to the concrete implementation
 builder.Services.AddHttpClient<ID4DataService, GraphQLD4DataService>
-            (spds =>
+            (dimensionFourDataService =>
             {
-                spds.BaseAddress = new Uri(builder.Configuration["DimensionFour:ApiUrl"]);
-                spds.DefaultRequestHeaders.Add(builder.Configuration["DimensionFour:Header1Name"], builder.Configuration["DimensionFour:Header1Value"]);
-                spds.DefaultRequestHeaders.Add(builder.Configuration["DimensionFour:Header2Name"], builder.Configuration["DimensionFour:Header2Value"]);
+                dimensionFourDataService.BaseAddress = new Uri(builder.Configuration["DimensionFour:ApiUrl"]);
+                dimensionFourDataService.DefaultRequestHeaders.Add(builder.Configuration["DimensionFour:Header1Name"], builder.Configuration["DimensionFour:Header1Value"]);
+                dimensionFourDataService.DefaultRequestHeaders.Add(builder.Configuration["DimensionFour:Header2Name"], builder.Configuration["DimensionFour:Header2Value"]);
             });
 
 var app = builder.Build();
--- FM4017Library/DataServices/SpaceX/ISpaceXDataService.cs
+++
@@ -1,8 +0,0 @@
-﻿using FM4017Library.Dtos.SpaceX;
-
-namespace FM4017Library.DataServices.SpaceX;
-
-public interface ISpaceXDataService
-{
-    Task<LaunchDto[]> GetAllLaunches();
-}

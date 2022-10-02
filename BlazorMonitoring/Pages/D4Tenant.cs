0f5310e1b97e0d641b242fa3f60e223d416fda63
EiK <231216@usn.no>
2022-10-02 21:26:49 +0200

    Removed unused code, improved visual of tables

--- BlazorMonitoring/Pages/D4Tenant.cs
+++ BlazorMonitoring/Pages/D4Tenant.cs
@@ -9,34 +9,14 @@ public partial class D4Tenant
     [Inject]
     ID4DataService? D4DataService { get; set; }
 
-
-
+    private string? _d4TenantName;
     private List<SpaceNode>? spaceNodes;
-    //private List<PointNode>? pointNodes;
-    //private List<SignalNode>? signalNodes;
-
-    private List<SpaceNode>? _selectedSpaceNodes;
-
 
     protected override async Task OnInitializedAsync()
     {
+        _d4TenantName = _config["DimensionFour:Header1Value"];
+
         spaceNodes = await D4DataService.GetAllSpacesPointsSignals();
-
-        // Get spaces with no parent
-        _selectedSpaceNodes = SelectSpaceNodes(null);
     }
 
-    /// <summary>
-    /// List Spaces where parentId equals param
-    /// </summary>
-    /// <param name="parentId"></param>
-    /// <returns></returns>
-    private List<SpaceNode>? SelectSpaceNodes(string parentId)
-    {
-        var result = (from spaceNode in spaceNodes
-                      where spaceNode.Parent?.Id == parentId
-                      select spaceNode).ToList();
-
-        return result;
-    }
 }

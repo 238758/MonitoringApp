4326b260b5e49fc3cb2c8f28b5199b21492a28b1
EiK <231216@usn.no>
2022-10-05 23:45:38 +0200

    Changed display time from Utc to local

---
+++ FM4017Library/Helpers/DateTimeHelpers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FM4017Library.Helpers;
+
+public static class DateTimeHelpers
+{
+    /// <summary>
+    /// Convert time from utc to local time
+    /// </summary>
+    /// <param name="utcTime"></param>
+    /// <returns></returns>
+    public static DateTime? UtcTime2Local(DateTime? utcTime)
+    {
+        if (utcTime != null)
+        {
+            return TimeZoneInfo.ConvertTimeFromUtc(utcTime.Value, TimeZoneInfo.Local);
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+
+}
+
+

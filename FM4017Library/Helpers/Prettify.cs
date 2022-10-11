632624f958bdd1134d85d23b3e60b7427ee1e15e
EiK <231216@usn.no>
2022-10-11 22:13:35 +0200

    improved appearance

---
+++ FM4017Library/Helpers/Prettify.cs
@@ -0,0 +1,23 @@
+﻿namespace FM4017Library.Helpers;
+
+public static class Prettify
+{
+    public static string? Unit(string? unit)
+    {
+        if (unit == "CELSIUS_DEGREES")
+        {
+            return "°C";
+        }
+        else if (unit == "PERCENTS")
+        {
+            return "% RH";
+        }
+
+        return unit;
+    }
+
+    public static string? Name(string? name)
+    {
+        return name?.Replace("_", " ");
+    }
+}

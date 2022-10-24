0778f2fea80ced1d039b71378aac10a4767c75f5
EiK <231216@usn.no>
2022-10-24 18:42:13 +0200

    added unit tests

---
+++ FM4017LibraryTests/Helpers/PrettifyTests.cs
@@ -0,0 +1,39 @@
+﻿using FM4017Library.Helpers;
+
+namespace FM4017LibraryTests.Helpers;
+
+[TestClass]
+public class PrettifyTests
+{
+    [TestMethod]
+    public void UnitTest()
+    {
+        string? actual, expected;
+
+        actual = Prettify.Unit("CELSIUS_DEGREES");
+        expected = "°C";
+        Assert.AreEqual(expected, actual);
+
+        actual = Prettify.Unit("PERCENTS");
+        expected = "% RH";
+        Assert.AreEqual(expected, actual);
+
+        actual = Prettify.Unit("%");
+        expected = "%";
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void NameTest()
+    {
+        string? actual, expected;
+
+        actual = Prettify.Name("Some_Name");
+        expected = "Some Name";
+        Assert.AreEqual(expected, actual);
+
+        actual = Prettify.Name("Some Name");
+        expected = "Some Name";
+        Assert.AreEqual(expected, actual);
+    }
+}
